3f9081a687e9b59e8ed5f4c6ba876a35ea3df91f
Nemurigi <ne6rigi@gmail.com>
2026-01-20 01:59:41 +0900

    ライセンスを追加

--- Packages/com.nemurigi.poiyomi-style-glitter-for-liltoon/Shaders/custom_insert.hlsl
+++ Packages/com.nemurigi.poiyomi-style-glitter-for-liltoon/Shaders/custom_insert.hlsl
@@ -1,7 +1,7 @@
 // MIT License
 //
 // Copyright (c) 2023 Poiyomi Inc.  (original author)
-// Copyright (c) 2025 nemurigi (ported to liltoon custom shader)
+// Copyright (c) 2026 nemurigi (ported to liltoon custom shader)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
